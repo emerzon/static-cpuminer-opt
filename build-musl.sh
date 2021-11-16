12ece961d5496c9fe4b88ffc86c3150dc76f4e0c
Emerson Gomes <emerson.gomes@gmail.com>
2021-11-16 22:50:25 +0100

    disabled pgo (to be reimplemented as a flag) added new cpuminer-gr to musl build

--- build-musl.sh
+++ build-musl.sh
@@ -4,7 +4,7 @@ CID=$(sudo docker create cpu)
 hash=$(cat /proc/cpuinfo | grep flags | uniq | md5sum | cut -b 1-8)
 mkdir -p artifacts
 sudo docker cp ${CID}:/usr/src/cpuminer-opt/cpuminer artifacts/cpuminer-opt-musl-${hash}
-#sudo docker cp ${CID}:/usr/src/cpuminer-gr/cpuminer artifacts/cpuminer-gr-${hash}
+sudo docker cp ${CID}:/usr/src/cpuminer-gr-avx2/cpuminer artifacts/cpuminer-gr-musl-${hash}
 #sudo docker cp ${CID}:/usr/src/xmrig/build/xmrig artifacts/xmrig-${hash}
 #sudo docker cp ${CID}:/usr/src/xmrig-upx/build/xmrig artifacts/xmrig-upx-${hash}
 sudo docker rm ${CID}
